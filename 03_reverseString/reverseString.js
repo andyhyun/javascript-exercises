3bc6a43fc24320eba11744c51eb43ea8a5197289
andyhyun <ak2572@njit.edu>
2022-08-11 14:12:29 -0400

    Add reverse string solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+  result = "";
 
+  for (let i = 1; i <= string.length; i++) {
+    result += string.charAt(string.length - i);
+  }
+
+  return result;
 };
 
 // Do not edit below this line
