5cfc175b69493688aedbe4f8ad64dd80fc83742b
andyhyun <ak2572@njit.edu>
2022-08-11 17:07:18 -0400

    Add remove from array solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...items) {
+  for (const item of items) {
+    itemIndex = arr.indexOf(item);
+    if (itemIndex !== -1) {
+      arr.splice(itemIndex, 1);
+    }
+  }
 
+  return arr;
 };
 
 // Do not edit below this line
