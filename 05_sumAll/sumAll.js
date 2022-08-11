f383169d29b3d4fb45f41d8ce50a2b8f02fbe7ea
andyhyun <ak2572@njit.edu>
2022-08-11 17:21:03 -0400

    Add sum all solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(low, high) {
+  if (high < low) {
+    return sumAll(high, low);
+  }
 
+  if (
+    typeof low !== 'number' ||
+    typeof high !== 'number' ||
+    low < 0 ||
+    high < 0
+  ) {
+    return "ERROR";
+  }
+
+  let sum = 0;
+
+  for (let i = low; i <= high; i++) {
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
