b66f631f6f0d46ec65a98625399d0f6d0971a7d1
andyhyun <ak2572@njit.edu>
2022-08-16 21:51:27 -0400

    Add palindromes solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  newStr = str.replace(/[^a-z]/gi, '').toLowerCase();
+  for (let i = 0; i < newStr.length; i++) {
+    if (newStr.charAt(i) !== newStr.charAt(newStr.length - i - 1)) {
+      return false;
+    }
+  }
+  return true;
 };
 
 // Do not edit below this line
