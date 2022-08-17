8281fb00f60966f9e0e8e8a34f6a8f6a643dc35e
andyhyun <ak2572@njit.edu>
2022-08-16 22:14:40 -0400

    Add fibonacci solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+  n = parseInt(n);
+  if (n < 0) return "OOPS";
+  if (n < 2) return n;
+  let firstPreceding = 1;
+  let secondPreceding = 0;
+  let result = 0;
+  for (let i = 2; i <= n; i++) {
+    result = firstPreceding + secondPreceding;
+    secondPreceding = firstPreceding;
+    firstPreceding = result;
+  }
+  return result;
 };
 
 // Do not edit below this line
